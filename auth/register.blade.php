8b305c1ea2c8864c9a36502c33fce4385005df26
Matheus de Souza Barbosa <60266964+matheusdesouzab@users.noreply.github.com>
2023-04-19 07:48:45 -0300

    Create register.blade.php

---
+++ auth/register.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Registrar</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"type="text/css">
+</head>
+
+<body class="">
+    <div id="authForm">
+        <form method="" action="" id="formLogin">
+            <div class="logo">
+                <h1>Simple Tasks</span></h1>
+                <img src="https://www.pngall.com/wp-content/uploads/12/Green-Check-PNG-Cutout.png" alt="">
+            </div>
+            <p class="description">Crie sua conta agora em nossa plataforma, e começe a organizar suas tarefas de forma
+                bastante simples.</p>
+            <label for="email">E-mail</label>
+            <input type="email" name="email" placeholder="Digite seu e-mail" />
+            <label for="name">Nome</label>
+            <input type="text" name="name" placeholder="Digite seu nome" />
+            <label for="password">Senha</label>
+            <input type="password" name="password" placeholder="Digite sua senha" />
+            <a href="" style="margin-top: 10px;">Já possui uma conta? Acessar agora</a>
+            <button class="btn" type="">Criar conta</button>
+        </form>
+    </div>
+
+</body>
+
+</html>
