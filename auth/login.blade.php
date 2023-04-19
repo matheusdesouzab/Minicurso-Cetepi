8c3429ab611d96a87bf9a63aa9ca1468784a6b86
Matheus de Souza Barbosa <60266964+matheusdesouzab@users.noreply.github.com>
2023-04-19 07:54:49 -0300

    Create login.blade.php

---
+++ auth/login.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"type="text/css">
+</head>
+
+<body>
+    <div id="authForm">
+        <form method="" action="">
+            <div class="logo">
+                <h1>Simple Tasks</span></h1>
+                <img src="https://www.pngall.com/wp-content/uploads/12/Green-Check-PNG-Cutout.png" alt="">
+            </div>
+            <p class="description">Acesse sua conta e continue organizando suas tarefas</p>
+            <label for="email">E-mail</label>
+            <input type="email" name="email" placeholder="Digite seu e-mail" value="" />
+            <label for="password">Senha</label>
+            <input type="password" name="password" placeholder="Digite sua senha" value="" />
+            <a href="" style="margin-top: 10px;">Não tem uma conta? Criar agora</a>
+            <button class="btn" type="submit">Acessar conta</button>
+        </form>
+    </div>
+</body>
+
+</html>
