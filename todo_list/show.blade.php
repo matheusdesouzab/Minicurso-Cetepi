35655da9f0bf2129dc63cb471ad45d06864cc396
Matheus de Souza Barbosa <60266964+matheusdesouzab@users.noreply.github.com>
2023-04-19 21:17:32 -0300

    Create show.blade.php

---
+++ todo_list/show.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dados da lista</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+</head>
+
+<body class="">
+
+    <nav>
+        <div class="logo">
+            <a class="{{ route('todo-list.index') }}" href="">SIMPLES TASKS</a>
+            <img src="https://www.pngall.com/wp-content/uploads/12/Green-Check-PNG-Cutout.png" alt="">
+        </div>
+        <ul>
+            <li><a href="#" onclick="document.getElementById('logout').submit()"><i
+                        class="bi bi-box-arrow-right"></i></a></li>
+            <form id="logout" action="{{ route('logout') }}" method="POST">
+                @csrf
+            </form>
+        </ul>
+    </nav>
+
+    <main id="container">
+        <div class="content-header">
+        
+        </div>
+    </main>
+
+</html>
