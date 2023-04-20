bef28b1a05b0417a581e5100c81fe5b339508efb
Matheus de Souza Barbosa <60266964+matheusdesouzab@users.noreply.github.com>
2023-04-19 21:26:20 -0300

    Create 403.blade.php

---
+++ errors/403.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Não autorizado</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"type="text/css">
+</head>
+
+<body>
+    <div id="authForm">
+        Você não tem autorização para acessar esse componente
+    </div>
+</body>
+
+</html>
