0554d7602799175d64ef7bd951792d1e924b55f5
Matheus de Souza Barbosa <60266964+matheusdesouzab@users.noreply.github.com>
2023-05-28 19:03:55 -0300

    Create table.blade.php

---
+++ todo_list/table.blade.php
@@ -0,0 +1,15 @@
+<div class="table-responsive">
+<table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome da tarefa</th>
+                    <th>Data de criação</th>
+                </tr>
+            </thead>
+            <tbody>
+               
+            </tbody>
+        </table>
+    </div>
+    </table
