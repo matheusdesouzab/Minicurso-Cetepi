ea908eb5a5beb014b1cefbb5ca9b2839334d530c
Matheus de Souza Barbosa <60266964+matheusdesouzab@users.noreply.github.com>
2023-05-28 16:43:15 -0300

    Create home.blade.php

---
+++ home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"type="text/css">
+</head>
+
+<body class="">
+    <nav>
+        <div class="logo">
+            <a class="" href="">SIMPLES TASKS</a>
+            <img src="https://www.pngall.com/wp-content/uploads/12/Green-Check-PNG-Cutout.png" alt="">
+        </div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Nova Tarefa</a></li>
+            <li><a href="#">Listagem</a></li>
+            <li><a href="#">Sair</a></li>
+        </ul>
+    </nav>
+
+    <main id="container">
+        
+    </main>
+
+</html>
